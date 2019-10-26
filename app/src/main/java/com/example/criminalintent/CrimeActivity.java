a2bd6c8476da9166ed5df5e5131e47cb5491f6b5
Tim Kao <pakuya.kao@gmail.com>
2019-10-26 12:41:06 -0700

    link activity with fragment through fragment manager

--- app/src/main/java/com/example/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/example/criminalintent/CrimeActivity.java
@@ -7,6 +7,8 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.View;
 import android.view.Menu;
@@ -18,6 +20,16 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 
 }
