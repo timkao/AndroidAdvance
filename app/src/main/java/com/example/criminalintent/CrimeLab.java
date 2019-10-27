78b8c1ae10f6705c5ea579877c6e87f024f361fb
Tim Kao <pakuya.kao@gmail.com>
2019-10-27 16:56:28 -0700

    slight adjustment of the outlook

--- app/src/main/java/com/example/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/criminalintent/CrimeLab.java
@@ -23,7 +23,7 @@ public class CrimeLab {
             Crime crime = new Crime();
             crime.setTitle("Crime #" + i);
             crime.setSolved(i % 2 == 0);
-            if (i % 11 == 0) {
+            if (i % 11 == 12) {
                 crime.setRequiredPolice(true);
             }
             mCrimes.add(crime);
