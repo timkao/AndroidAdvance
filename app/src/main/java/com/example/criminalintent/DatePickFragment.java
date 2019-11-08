6901c4d49607269d5b2f5a73a95c0027de63899a
Tim Kao <pakuya.kao@gmail.com>
2019-11-07 21:34:55 -0800

    create a date picker view and added it to the dialog fragment

--- app/src/main/java/com/example/criminalintent/DatePickFragment.java
+++ app/src/main/java/com/example/criminalintent/DatePickFragment.java
@@ -3,16 +3,23 @@ package com.example.criminalintent;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 public class DatePickFragment extends DialogFragment {
+
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
